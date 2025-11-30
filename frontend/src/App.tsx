66898bea3f3b84687f6890000c4d491b989170f5
Eliman-C5 <eliman1926@gmail.com>
2025-11-30 11:15:31 -0300

    Set frontend config and styles

---
+++ frontend/src/App.tsx
@@ -0,0 +1,24 @@
+// import PricingTable from './components/PricingClient';
+// import Sidebar from './components/Sidebar';
+import { useEffect, useState } from 'react';
+import { useGetPlants } from './hooks/useGetPlants';
+
+export const App = () => {
+  const [currentPlant, setCurrentPlant] = useState({ id: '', name: '' });
+
+  const { plants, loading } = useGetPlants();
+
+  useEffect(() => {
+    if (plants.length === 0) return;
+    setCurrentPlant({ id: plants[0].id || '', name: plants[0].name || '' });
+  }, [plants]);
+
+  return (
+    <div className="flex h-screen bg-background">
+      {/* <Sidebar plants={plants} currentPlant={currentPlant} setCurrentPlant={setCurrentPlant} loading={loading} /> */}
+      <main className="flex-1 overflow-auto">
+        {/* <PricingTable plantId={currentPlant.id} /> */}
+      </main>
+    </div>
+  );
+};
---
+++ frontend/src/main.tsx
@@ -0,0 +1,23 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { ApolloClient } from '@apollo/client';
+import { InMemoryCache } from '@apollo/client';
+import { HttpLink } from '@apollo/client/link/http';
+import { ApolloProvider } from '@apollo/client/react';
+import { App } from './App.tsx';
+
+const link = new HttpLink({ uri: 'http://localhost:4001/graphql' });
+
+const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache(),
+});
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </StrictMode>,
+);
